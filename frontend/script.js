8d9958fea4b2221df4ca5d5c2baa964d1cf16837
Abhinav Goud <abhinavgoud560@gmail.com>
2026-09-14 21:57:16 +0530

    Added student registration

--- frontend/script.js
+++ frontend/script.js
@@ -193,5 +193,54 @@ async function rejectFaculty(id) {
 // INITIAL LOAD
 // ======================================
 
-loadAdmin();
-loadFaculty();
+// Load Admin Page Only
+if (document.getElementById("adminStudents")) {
+    loadAdmin();
+}
+
+// Load Faculty Page Only
+if (document.getElementById("facultyTable")) {
+    loadFaculty();
+}async function registerStudent() {
+
+    const student = {
+        roll_no: document.getElementById("roll").value,
+        name: document.getElementById("name").value,
+        department: document.getElementById("department").value,
+        year: document.getElementById("year").value,
+        section: document.getElementById("section").value,
+        password: document.getElementById("password").value
+    };
+
+    try {
+
+        const response = await fetch(API + "/students/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(student)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+
+            alert("Registration Successful!");
+
+            window.location.href = "login.html";
+
+        } else {
+
+            alert(data.detail || "Registration Failed");
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Cannot connect to backend.");
+
+    }
+}
