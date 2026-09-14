51f5988d34bc161ce0fe3abb8ca026c1607aa664
Abhinav Goud <abhinavgoud560@gmail.com>
2026-09-14 21:28:51 +0530

    Update CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,8 +15,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    https://hitam-smart-ai-doubt-portal.vercel.app/,
+],
     ],
     allow_credentials=True,
     allow_methods=["*"],
